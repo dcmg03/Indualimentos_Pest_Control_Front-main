253ff3b4ccb575ba25e0b445a462d372612681ed
Julieth-Becerra <julieth.becerra01@uptc.edu.co>
2023-09-22 15:25:01 -0500

    Agregar tipo de servicio y Agenda

--- src/app/components/agenda/agenda.component.ts
+++ src/app/components/agenda/agenda.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AgendaService} from './agenda.service';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-agenda',
@@ -8,17 +9,52 @@ import {AgendaService} from './agenda.service';
 })
 export class AgendaComponent implements OnInit {
   programacion: any[]=[];
-  date:any; 
+  date:any;
 
-  constructor(private agendaService: AgendaService) {}
+  public servicios = [];
+  public servicioSeleccionado: any;
+
+  public tipoServicios = [];
+  public tipoServicioSeleccionado: any;
+
+  public availableHours: any[] = [];
+  public selectedHours: any;
+
+  constructor(private agendaService: AgendaService, private apiService: ApiService) {}
 
   ngOnInit() {
-    // Recupera la programación del día desde el servicio de Agenda
-    this.programacion = this.agendaService.obtenerProgramacionDelDia();
+    this.generateAvailableHours();
+    this.apiService.getAll("service").subscribe(res => {
+      this.servicios = res as []
+    });
+  }
+
+  private generateAvailableHours() {
+    let hour = 8;
+    let option = 0; // Comenzamos a las 8 AM
+
+    while (hour <= 18) { // Terminamos a las 6 PM
+      for (let minutes = 0; minutes < 60; minutes += 30) {
+        const formattedHour = `${hour.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+        this.availableHours.push({id: option, name: formattedHour});
+      }
+      option ++
+      hour++;
+    }
+  }
+
+  changeOptions(value: any){
+    this.apiService.getAllByFilters("kindOfService", {
+      service: {
+        id: value
+      }
+    }).subscribe(res => {
+      this.tipoServicios = res as [];
+    })
+
   }
 
   cancelarServicio(servicio: any) {
-    // Implementa la lógica para cancelar el servicio aquí, por ejemplo, marcando el servicio como cancelado en el servicio de Agenda.
     this.agendaService.cancelarServicio(servicio.id);
   }
 }
