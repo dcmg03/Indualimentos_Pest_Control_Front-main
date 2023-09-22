42d60e72e1bc8bcc5f4878cc711a45528e2eb3a4
Julieth-Becerra <julieth.becerra01@uptc.edu.co>
2023-09-22 09:41:04 -0500

    items menuBar segun role

--- src/app/components/menubar/menubar.component.ts
+++ src/app/components/menubar/menubar.component.ts
@@ -5,31 +5,62 @@ import {Component} from '@angular/core';
   styleUrls: ['./menubar.component.css']
 })
 export class MenubarComponent {
+
+  role:any
   items: any;
 
   constructor() {
-    this.items = [
-      {label: 'Inicio', icon: 'pi pi-home', routerLink: '/admin'},
-      {
-        label: 'Empleados', icon: 'pi pi-users',
-        items: [
-          {label: 'Ver Empleados', icon: 'pi pi-verempleado', routerLink: '/verempleado'},
-          {label: 'Agregar Empleado', icon: 'pi pi-agempleado', routerLink: '/agregarempleado'}
-        ]
-      },
-      {
-        label: 'Servicios',
-        icon: 'pi pi-briefcase',
-        items: [
-          {label: 'Ver Servicios', icon: 'pi pi-list', routerLink: '/servicios'},
-          {label: 'Agregar Servicios', icon: 'pi pi-plus', routerLink: '/agregarservicio'}
-        ]
-      },
 
-      {label: 'Clientes', icon: 'pi pi-clientes', routerLink: '/clientes'},
-      {label: 'Agenda',icon: 'pi pi- agenda', routerLink: '/agenda'
-      }
-    ];
+  }
+
+  ngOnInit() {
+    this.role = localStorage.getItem("role");
+    console.log(this.role);
+
+    if (this.role === "E") {
+      this.items = [
+        { label: 'Inicio', icon: 'pi pi-home', routerLink: '/admin' },
+        {
+          label: 'Servicios',
+          icon: 'pi pi-briefcase',
+          items: [{ label: 'Ver Servicios', icon: 'pi pi-list', routerLink: '/servicios' }]
+        },
+        { label: 'Clientes', icon: 'pi pi-clientes', routerLink: '/clientes' },
+        { label: 'Agenda', icon: 'pi pi-agenda', routerLink: '/agenda' }
+      ];
+    }
+
+    if (this.role === "C") {
+      this.items = [
+        { label: 'Inicio', icon: 'pi pi-home', routerLink: '/admin' },
+        {
+          label: 'Servicios',
+          icon: 'pi pi-briefcase',
+          items: [{ label: 'Ver Servicios', icon: 'pi pi-list', routerLink: '/servicios' }]
+        },
+        { label: 'Agenda', icon: 'pi pi-agenda', routerLink: '/agenda' }
+      ];
+    }
+
+    if (this.role === "A") {
+      this.items = [
+        { label: 'Inicio', icon: 'pi pi-home', routerLink: '/admin' },
+        {
+          label: 'Empleados', icon: 'pi pi-users',
+          items: [
+            { label: 'Ver Empleados', icon: 'pi pi-verempleado', routerLink: '/verempleado' },
+            { label: 'Agregar Empleado', icon: 'pi pi-agempleado', routerLink: '/agregarempleado' }
+          ]
+        },
+        {
+          label: 'Servicios',
+          icon: 'pi pi-briefcase',
+          items: [{ label: 'Ver Servicios', icon: 'pi pi-list', routerLink: '/servicios' }]
+        },
+        { label: 'Clientes', icon: 'pi pi-clientes', routerLink: '/clientes' },
+        { label: 'Agenda', icon: 'pi pi-agenda', routerLink: '/agenda' }
+      ];
+    }
   }
 
 }
