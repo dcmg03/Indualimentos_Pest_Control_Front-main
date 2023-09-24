12497999d57ccfe04d9863b7b84515ccdc771f8b
DonovanPS <86479462+Julieth-Becerra@users.noreply.github.com>
2023-09-24 10:46:52 -0500

    Corrección update tipo de servicio

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -125,16 +125,16 @@ export class ServiciosComponent implements OnInit {
     );
   }
 
-  fetchDataKindOf() {
+  fetchDataKindOf(){
     this.apiService.getAll("kindOfService").subscribe(
       (response: any) => {
-        
+
         this.tipoServicios = response.map((item: any) => ({
           id: item.id,
           name: item.name,
           value: item.value,
           service_id: item.service.id,
-          service_name: item.service.name,
+          service_name: item.service.name, // Asigna el valor de service_name desde item.service.name
         }));
 
 
@@ -144,7 +144,6 @@ export class ServiciosComponent implements OnInit {
       }
     );
   }
-
 
   delete(service: Servicio) {
     this.apiService.delete("service", service.id).subscribe(res => {
@@ -181,13 +180,15 @@ export class ServiciosComponent implements OnInit {
 
   updateKindOf(tipoServicio: any) {
 
+    console.log(tipoServicio);
+
 
     this.labelBoton = "Actualizar Tipo de Servicio";
 
     this.newTipoService.id = tipoServicio.id;
     this.newTipoService.name = tipoServicio.name;
     this.newTipoService.value= tipoServicio.value;
-    this.newTipoService.service.id = tipoServicio.service.id;
+    this.newTipoService.service.id = tipoServicio.service_id;
 
     this.visible2 = true;
 
